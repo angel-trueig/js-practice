4f0576a1c0cd8204efbbadf14db6a25b9f367c7b
angel-trueig <agurjar@trueigtech.com>
2026-01-15 12:01:06 +0530

    feat: merged with changes

--- script.js
+++ script.js
@@ -55,37 +55,4 @@ console.log(first);
 const[,,,fourth]= skills;
 console.log(fourth);
 
-const skills = ["JavaScript", "Python", "C++", "Java", "Ruby"];
 
-skills.push("Go"); //end
-skills.unshift("HTML"); //starting
-
-console.log(skills);
-
-skills.pop(); //remove last 
-console.log(skills);
-
-skills.shift();//remove first
-console.log(skills);
-
-skills.forEach(skill=>{
-    console.log(skill);
-})
-
-
-//destructuring
-const student = {
-    name:"Angel",
-    course:"Javascript",
-    duration:"3 months"
-};
-
-const {duration , name:username} = student;
-console.log(username);
-
-
-const [first , second] = skills;
-console.log(first);
-
-const[,,,fourth]= skills;
-console.log(fourth);
