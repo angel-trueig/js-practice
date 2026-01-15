3965ffe347445a07da8b5a9f07a52b9654cffc3f
angel-trueig <agurjar@trueigtech.com>
2026-01-15 11:48:38 +0530

    feat: dom practice

--- script.js
+++ script.js
@@ -1,27 +1,38 @@
-let btn = document.getElementById('addBtn');
-let taskInput = document.getElementById('taskInput');
-let taskList = document.getElementById('taskList');
+const title = document.getElementById("title");
+const description = document.querySelector(".description")
+const changeBtn = document.getElementById("changeBtn");
 
-btn.addEventListener('click', function() {
-    let input = taskInput.value;
-    if(input === ""){
-        alert("Please enter a task");
-        return;
+changeBtn.addEventListener("click", function(){
+    title.textContent = "DOM Manipulated!";
+    description.textContent = "You have successfully changed the text using JavaScript.";
+});
+
+const newPara = document.createElement("p");
+newPara.textContent = "This paragrapg was added using JS";
+
+document.body.appendChild(newPara);
+
+const input =document.getElementById("nameInput");
+const addBtn = document.getElementById("addBtn");
+const nameList = document.getElementById("nameList");
+
+addBtn.addEventListener("click", function(){
+    const addName = input.value;
+
+    if(addName === "")return;
+
+    const li = document.createElement("li");
+    li.textContent = addName;
+    nameList.appendChild(li);
+    input.value = "";
+});
+
+nameList.addEventListener("click", (e)=>{
+    if(e.target.tagName === "LI"){
+        e.target.style.textDecoration = "line-through";
     }
-    let li = document.createElement('li');
-    li.textContent = input;
-
-    let deleteBtn = document.createElement('button');
-    deleteBtn.textContent = "Delete";
-
-    li.appendChild(deleteBtn);
-    taskList.appendChild(li);
-
-    taskInput.value = "";
 })
 
-taskList.addEventListener("click" , function(event){
-    if(event.target.tagName === "BUTTON"){
-        event.target.parentElement.remove();
-    }
+nameInput.addEventListener("keydown" , function(e){
+    console.log(e.key);
 })
