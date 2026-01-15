b8256a61d330f6d2eda3a068b745a2e07369ce8b
angel-trueig <agurjar@trueigtech.com>
2026-01-15 11:53:53 +0530

    merged basic with arrays

--- script.js
+++ script.js
@@ -19,3 +19,38 @@ const userProfile = {
 }
 
 userProfile.greet();
+const skills = ["JavaScript", "Python", "C++", "Java", "Ruby"];
+
+skills.push("Go"); //end
+skills.unshift("HTML"); //starting
+
+console.log(skills);
+
+skills.pop(); //remove last 
+console.log(skills);
+
+skills.shift();//remove first
+console.log(skills);
+
+skills.forEach(skill=>{
+    console.log(skill);
+})
+
+
+//destructuring
+const student = {
+    name:"Angel",
+    course:"Javascript",
+    duration:"3 months"
+};
+
+const {duration , name:username} = student;
+console.log(username);
+
+
+const [first , second] = skills;
+console.log(first);
+
+const[,,,fourth]= skills;
+console.log(fourth);
+
